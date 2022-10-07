fc7835efc6609728a5c8fd73263b23d56981a770
Lukas Roth <lukiroth@bluewin.ch>
2022-10-07 12:48:01 +0200

    Tested some LCD things, created a luki init and luki function for testing and playing around

--- source/main.c
+++ source/main.c
@@ -3,7 +3,8 @@
 #include <stdint.h>
 #include <limits.h>
 
-void DrawInterface(void);
+void LukiInit(void);
+void LukisFunction(void);
 
 int main()
 {
@@ -14,19 +15,55 @@ int main()
 	// Initialize LCD
 	LCD_Init();
 
-	LCD_Circle(50, 50, radius)
+	//--- Declarations
+
 
 	// Set logging output destination to be the LCD
 	LOG_SetDestination(LCD_Stream);
 
+	//--- Init functions
+	LukiInit();
+
 	//--- Main loop
 	while (true)
 	{
-		DrawInterface();
+		LukisFunction();
 	}
 }
 
-void DrawInterface(void)
+void LukiInit(void)
 {
+	//LCD_SetForegroundColor(ColorWhite);
+	//LCD_Rect(0, 0, 480, 480);
 
+	//Draw Down Button
+	LCD_SetForegroundColor(ColorRed);
+	LCD_String(0, 0, "Hello");
+	//Draw Up Button
+}
+
+void LukisFunction(void)
+{
+	static int x_old = 0;
+	static int y_old = 0;
+
+	LCD_TouchPosition_t touchPosition = {0, 0};
+	if (R_SUCCESS(LCD_TouchGetPosition(&touchPosition)))
+	{
+		int x_new = touchPosition.x;
+		int y_new = touchPosition.y;
+
+		if (x_new != 0 || y_new != 480)
+		{
+			LCD_SetForegroundColor(ColorWhite);
+			LCD_Rect(x_old - 5, y_old - 5, 30, 30);
+
+			LCD_SetForegroundColor(ColorRed);
+			LCD_Rect (x_new, y_new, 20, 20);
+
+			x_old = x_new;
+			y_old = y_new;
+		}
+	}
+	//CORE_Delay(200);
 }
